6bf64bcb63a95fa0aff00f9918ed6acb6a662827
fanoraprem <zydanvpn424@gmail.com>
2025-03-18 01:37:21 +0700

    Add files via upload

---
+++ menu_all/vless-menu.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# My Telegram 
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+DF='\e[39m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[33m'
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+PURPLE='\e[1;95m'
+CYAN='\e[1;36m'
+Lred='\e[1;91m'
+Lgreen='\e[92m'
+Lyellow='\e[93m'
+white='\e[1;37m'
+NC='\e[0m'
+# ==========================================
+clear
+x="ok"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[41;1;39m                     ⇱ XRAY MENU ⇲              \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+rekk='Xray'
+echo -e "
+[${GREEN}01${NC}] ${color1}•${NC} Create $rekk Vless Websocket Account
+[${GREEN}02${NC}] ${color1}•${NC} Deleting $rekk Vless Websocket Account
+[${GREEN}03${NC}] ${color1}•${NC} Extending $rekk Vless Account Active Life
+[${GREEN}04${NC}] ${color1}•${NC} Check User Login $rekk
+
+${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}
+
+[${GREEN}00${NC}] ${color1}• ${NC}Kembali Ke Menu \033[1;32m<\033[1;33m<\033[1;31m<\033[1;31m"
+echo ""
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[41;1;39m                  ⇱ HUMANITY PROJECT ⇲            \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e""
+read -p " Select menu :  "  v2ray
+echo -e ""
+case $v2ray in
+1 |01)
+add-vless
+;;
+2 |02)
+del-vless
+;;
+3 |03)
+renew-vless
+;;
+4 |04)
+cek-vless
+;;
+0 | 00)
+menu
+;;
+*)
+vless-menu
+;;
+esac
